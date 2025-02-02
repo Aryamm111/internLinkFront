d7328f0ae10f219e6c0a4ba970cb9a42ed072b8e
aryam <aryamm0002@gmail.com>
2025-02-02 17:02:46 +0300

    Added table and student info components, modified Main.jsx to work with routes

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -4,18 +4,6 @@ import { useUser } from '../UseContext.jsx';
 const TopNavBar = () => {
   const { userName } = useUser();
 
-  return (
-    <header className="bg-white shadow p-4 border-b border-gray-200 w-full fixed top-0 z-10">
-      <div className="max-w-screen-xl mx-auto flex  pr-6">
-       
-          <div >
-            <span className="text-gray-700 text-sm font-medium">{userName}</span>
-            <span className="text-gray-500 text-sm">Aryam@gmail.com</span>
-          </div>
-       
-      </div>
-    </header>
-  );
 };
 
 export default TopNavBar;
