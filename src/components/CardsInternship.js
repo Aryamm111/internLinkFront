38e83b539564b7955633e5dd92eb702edb523456
Aryamm111 <160420788+Aryamm111@users.noreply.github.com>
2025-02-13 00:32:28 +0300

    Create CardsInternship.js

---
+++ src/components/CardsInternship.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// const internshipsData = [
+//     {
+//       title: "Software Engineering Intern",
+//       company: "Tech Corp",
+//       location: "Riyadh",
+//       dates: "Jun 2025 - Aug 2025",
+//       description: "Work on cutting-edge software projects.",
+//       buttonText: "Apply Now",
+//       Status:"rejected",
+//       onClick: () => alert("Apply for Software Engineering Intern")
+//     },
+//     {
+//       title: "Marketing Intern",
+//       company: "Marketing Solutions",
+//       location: "Jeddah",
+//       dates: "Jul 2025 - Sep 2025",
+//       description: "Assist with marketing campaigns and strategies.",
+//       buttonText: "Apply Now",
+//       Status:"accepted",
+//       onClick: () => alert("Apply for Marketing Intern")
+//     }
+//   ];
+//   export default internshipsData;
